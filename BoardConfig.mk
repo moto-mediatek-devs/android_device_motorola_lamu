232a38bc33bd57ff1676568bfd388cb084387930
R0rt1z2 <roger@r0rt1z2.com>
2026-05-22 09:38:24 +0200

    lamu: Include prebuilt kernel files
    Change-Id: I0fcb35106e1e7966c8fc8be5e7b5402da20e6378

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,7 @@
 #
 
 DEVICE_PATH := device/motorola/lamu
+KERNEL_PATH := $(DEVICE_PATH)-kernel
 
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 8589934592
@@ -13,6 +14,28 @@ include device/motorola/mt6768-common/BoardConfigCommon.mk
 
 # Display
 TARGET_SCREEN_DENSITY := 400
+
+# Kernel
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/system_dlkm.modules.load))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_dlkm.modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_ramdisk.modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_ramdisk.modules.load.recovery))
+
+TARGET_PREBUILT_KERNEL := $(KERNEL_PATH)/$(BOARD_KERNEL_IMAGE_NAME)
+TARGET_BOARD_KERNEL_HEADERS := $(KERNEL_PATH)/kernel-headers
+BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
+
+ALL_VENDOR_RAMDISK_MODULES := $(sort $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD) $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD))
+BOARD_KERNEL_MODULE_DIR := $(KERNEL_PATH)/modules
+BOARD_SYSTEM_KERNEL_MODULES := $(addprefix $(BOARD_KERNEL_MODULE_DIR)/,$(BOARD_SYSTEM_KERNEL_MODULES_LOAD))
+BOARD_VENDOR_KERNEL_MODULES := $(addprefix $(BOARD_KERNEL_MODULE_DIR)/,$(BOARD_VENDOR_KERNEL_MODULES_LOAD))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(BOARD_KERNEL_MODULE_DIR)/,$(ALL_VENDOR_RAMDISK_MODULES))
+
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(KERNEL_PATH)/modules/gps_drv_stp.ko \
+    $(KERNEL_PATH)/modules/wlan_drv_gen4m_6768.ko \
+    $(KERNEL_PATH)/modules/wmt_chrdev_wifi.ko \
+    $(KERNEL_PATH)/modules/wmt_drv.ko
 
 # Recovery
 TARGET_RECOVERY_DENSITY := xhdpi
