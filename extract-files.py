935aa11ae2e4a3c7748d1fb1a531db4e461bfd44
mikeNG <mikeioannina@gmail.com>
2026-06-01 00:37:04 +0200

    lamu: Clear LIBNATIVEWINDOW symbol versions for libBSTSWAD
    Change-Id: I3820facfb4857ec5a264c94b47352fc4c72fdbf9

--- extract-files.py
+++ extract-files.py
@@ -24,6 +24,13 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
         .binary_regex_replace(b'A2dpsuspendonly', b'A2dpSuspended\x00\x00')
         .binary_regex_replace(b'BTAudiosuspend', b'A2dpSuspended\x00'),
+    'vendor/lib64/libBSTSWAD.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
     (
         'vendor/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so',
         'vendor/lib64/libmtkcam_grallocutils.so'
