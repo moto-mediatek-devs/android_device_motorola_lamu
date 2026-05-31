5a0972b11f29ba6a2beb424d03f5dd8784cb187a
R0rt1z2 <roger@r0rt1z2.com>
2026-06-01 00:27:53 +0200

    lamu: Patch camera libs to use graphics.common-V7-ndk
    Change-Id: I838fbb44d15e47e92e9a4b0205ca48d0644ada3f
    Signed-off-by: R0rt1z2 <roger@r0rt1z2.com>

--- extract-files.py
+++ extract-files.py
@@ -24,6 +24,11 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
         .binary_regex_replace(b'A2dpsuspendonly', b'A2dpSuspended\x00\x00')
         .binary_regex_replace(b'BTAudiosuspend', b'A2dpSuspended\x00'),
+    (
+        'vendor/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so',
+        'vendor/lib64/libmtkcam_grallocutils.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     'vendor/lib64/libmtkcam_hal_aidl_common.so': blob_fixup()
         .replace_needed('android.hardware.camera.common-V2-ndk.so', 'android.hardware.camera.common-V1-ndk.so'),
 }  # fmt: skip
