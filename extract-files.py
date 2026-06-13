a72be74cd2700a5a1a0320122c88f674941966b4
LuK1337 <priv.luk@gmail.com>
2026-06-13 18:08:37 +0200

    lamu: Setup firmware extraction
    Change-Id: Ibccc001659f24ef3cd32ee4b90b216bc275586f1

--- extract-files.py
+++ extract-files.py
@@ -47,6 +47,7 @@ module = ExtractUtilsModule(
     'motorola',
     blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
